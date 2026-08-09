5d23602d32fff747d1ed2fc493d7107727389f78
William A. <121400607+williamandrews2@users.noreply.github.com>
2026-08-09 16:10:51 -0500

    feat: populated table in admin  product page with data from db
    Inserted data from db into admin products table, show
    ing availabiliy, name, price, order count, and an action button for each product. Action b
    utton functionality not wired up yet.

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -4,10 +4,20 @@ import Link from "next/link";
 import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import prisma from "@/lib/prisma";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function AdminProductsPage() {
   return (
@@ -23,7 +33,20 @@ export default function AdminProductsPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: "asc" },
+  });
+
+  if (products.length === 0) return <p>No products found</p>;
+
   return (
     <Table>
       <TableHeader>
@@ -40,7 +63,49 @@ function ProductsTable() {
         </TableRow>
       </TableHeader>
 
-      <TableBody></TableBody>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <CheckCircle2 />
+                  <span className="sr-only">Available</span>
+                </>
+              ) : (
+                <>
+                  <XCircle />
+                  <span className="sr-only">Unvailable</span>
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <a download href={`/admin/products/${product.id}/download`}>
+                      Download
+                    </a>
+                  </DropdownMenuItem>
+
+                  <DropdownMenuItem>
+                    <Link href={`/admin/products/${product.id}/edit`}>
+                      Edit
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   );
 }
